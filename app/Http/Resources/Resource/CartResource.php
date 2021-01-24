8e96433b40d658283c25ce776737e2b0fd80fb43
moniem123 <mohammedabdelmoniem65@gmail.com>
2021-01-24 12:18:39 +0200

    change key

--- app/Http/Resources/Resource/CartResource.php
+++ app/Http/Resources/Resource/CartResource.php
@@ -78,7 +78,7 @@ class CartResource extends JsonResource
                     ],
                     'size' =>[
                         'id' => fix_null_string(optional(optional($q->productQuantity)->size)->id),
-                        'color' => fix_null_string(optional(optional($q->productQuantity)->size)->size),
+                        'size' => fix_null_string(optional(optional($q->productQuantity)->size)->size),
                     ],
                 ];
             });
