dc65f2d41b5de9b169e264a4e2e1e5689800e0bf
abdullahessam <eng.abdullah.essam@hotmail.com>
2021-01-27 14:18:34 +0200

    fix product colors

--- app/Http/Resources/Collection/ProductsCollection.php
+++ app/Http/Resources/Collection/ProductsCollection.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Collection;
 
 use App\Models\Product;
 use Illuminate\Http\Resources\Json\ResourceCollection;
-
+use App\Http\Resources\ProductQuantityCollection;
 /**
  * Class ProductsCollection
  * @package App\Http\Resources\Collection
@@ -33,7 +33,7 @@ class ProductsCollection extends ResourceCollection
                     'has_discount' => ((int)$q->discount>0),
                     'price_after_discount' => $q->price_after_discount,
                     'is_favourite'=>(auth()->check())?$user->isFavourite($q->id):false,
-                    'colors' => new ProductColorsCollection($q->product_colors),
+                    'colors' => new ProductColorsCollection($q->product_colors,$this),
 //                    'sizes' => new ProductSizesCollection($q->product_sizes),
                     'is_new' => ($q->is_new===0)?false:true,
 
@@ -51,7 +51,7 @@ class ProductsCollection extends ResourceCollection
                     'has_discount' => ((int)$q->discount>0),
                     'price_after_discount' => $q->price_after_discount,
                     'is_favourite'=>(auth()->check())?$user->isFavourite($q->id):false,
-                    'colors' => new ProductColorsCollection($q->product_colors),
+                    'colors' => new ProductColorsCollection($q->product_colors,$this),
 //                    'sizes' => new ProductSizesCollection($q->product_sizes),
                     'is_new' => ($q->is_new===0)?false:true,
                 ];
