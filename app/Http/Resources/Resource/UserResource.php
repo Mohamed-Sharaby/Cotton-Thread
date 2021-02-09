0956673e6ece5bce4a3dece1e916a208d8d42363
moniem123 <mohammedabdelmoniem65@gmail.com>
2021-02-09 15:25:46 +0200

    is-ban

--- app/Http/Resources/Resource/UserResource.php
+++ app/Http/Resources/Resource/UserResource.php
@@ -26,6 +26,7 @@ class UserResource extends JsonResource
             'email'=>$this->email,
             'phone'=>$this->phone,
             'is_verified'=>$this->is_verified,
+            'is_ban'=>($this->is_ban)?true:false,
             'token'=>fix_null_string($this->token),
             'image'=>fix_null_string($this->image),
         ];
