6abce52e5f85b9fa60a00db50bbab91452c7c9ee
mohamed.sharaby <moh.sharaby@yahoo.com>
2021-02-14 10:56:49 +0200

    rates

--- resources/views/site/products/product-rates.blade.php
+++ resources/views/site/products/product-rates.blade.php
@@ -36,7 +36,7 @@
 
     <div class="col-md-6 col-xs-12">
         @auth()
-            @if($can_rate)
+{{--            @if($can_rate)--}}
                 <div class="your-rating">
                     <form class="add_comment_form" action="{{route('website.products.rate')}}" method="post">
                         @csrf
@@ -48,24 +48,24 @@
                                 <fieldset class="rating">
                                     <input type="radio" id="star5" name="rating" value="5"/>
                                     <label class="full" for="star5" title="Awesome - 5 stars"></label>
-                                    <input type="radio" id="star4half" name="rating" value="4.5"/>
-                                    <label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>
+{{--                                    <input type="radio" id="star4half" name="rating" value="4.5"/>--}}
+{{--                                    <label class="half" for="star4half" title="Pretty good - 4.5 stars"></label>--}}
                                     <input type="radio" id="star4" name="rating" value="4"/>
                                     <label class="full" for="star4" title="Pretty good - 4 stars"></label>
-                                    <input type="radio" id="star3half" name="rating" value="3.5"/>
-                                    <label class="half" for="star3half" title="Meh - 3.5 stars"></label>
+{{--                                    <input type="radio" id="star3half" name="rating" value="3.5"/>--}}
+{{--                                    <label class="half" for="star3half" title="Meh - 3.5 stars"></label>--}}
                                     <input type="radio" id="star3" name="rating" value="3"/>
                                     <label class="full" for="star3" title="Meh - 3 stars"></label>
-                                    <input type="radio" id="star2half" name="rating" value="2.5"/>
-                                    <label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>
+{{--                                    <input type="radio" id="star2half" name="rating" value="2.5"/>--}}
+{{--                                    <label class="half" for="star2half" title="Kinda bad - 2.5 stars"></label>--}}
                                     <input type="radio" id="star2" name="rating" value="2"/>
                                     <label class="full" for="star2" title="Kinda bad - 2 stars"></label>
-                                    <input type="radio" id="star1half" name="rating" value="1.5"/>
-                                    <label class="half" for="star1half" title="Meh - 1.5 stars"></label>
+{{--                                    <input type="radio" id="star1half" name="rating" value="1.5"/>--}}
+{{--                                    <label class="half" for="star1half" title="Meh - 1.5 stars"></label>--}}
                                     <input type="radio" id="star1" name="rating" value="1"/>
                                     <label class="full" for="star1" title="Sucks big time - 1 star"></label>
-                                    <input type="radio" id="starhalf" name="rating" value=".5"/>
-                                    <label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+{{--                                    <input type="radio" id="starhalf" name="rating" value=".5"/>--}}
+{{--                                    <label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>--}}
                                 </fieldset>
                             </div>
                         </div>
@@ -75,7 +75,7 @@
                         <button type="submit" class="btn-hvr" id="add_comment_btn">إضافة</button>
                     </form>
                 </div>
-            @endif
+{{--            @endif--}}
         @else
             <h3>إضافة تقييمك</h3>
             <p class="alert alert-danger">سجل <a href="{{route('login')}}" class="text-light"> دخولك </a> لكى تتمكن من
